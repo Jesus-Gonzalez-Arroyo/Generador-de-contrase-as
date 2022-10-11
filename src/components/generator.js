b7b1daa74e3a9a1d78d3203b8e838501f75c690b
Jesus <jesusdavidgonzales1102@gmail.com>
2022-10-10 21:56:46 -0500

    Version 1

---
+++ src/components/generator.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Generador(){
+    return(
+        <div>
+            <div>
+                <input type='text' id="input"/>
+            </div>
+            <div>
+                <button type="button" id="button_generator">Generar</button>
+            </div>
+        </div>
+    )
+}
+
+
